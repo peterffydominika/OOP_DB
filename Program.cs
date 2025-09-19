fd3c0ad49cd49c34c9b18075ee19c8e166dbd322
peterffydominika <peterffy.dominika@gmail.com>
2025-09-19 10:25:51 +0200

    Lista kiírása.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using OOP_DB.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,16 @@ namespace OOP_DB
             Console.WriteLine("Kérem az adatbázis nevét: ");
             string dbName = Console.ReadLine();
             Connect conn = new Connect(dbName);
+            ILibrary dataBase = new Library();
+            dataBase.GetAllData(dbName);
+
+            
+
+            foreach (var item in dataBase.GetAllData(dbName)) {
+                var books = item.GetType().GetProperties();
+                Console.WriteLine($"Id= {books[0].Name}={books[0].GetValue(item)}, Title= {books[1].Name}={books[1].GetValue(item)}");
+            }
+            Console.WriteLine(dataBase.GetAllData(dbName));
         }
     }
 }
---
+++ Services/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OOP_DB.Services
+{
+    internal class Library : ILibrary
+    {
+        public List<object> GetAllData(string dbName)
+        {
+            Connect conn = new Connect("library");
+            List<object> books = new List<object>();
+            conn.Connnection.Open();
+            string sql = "SELECT * FROM books";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connnection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            while (dr.Read())
+            {
+                var book = new
+                {
+                    id = dr.GetInt32("id"),
+                    title = dr.GetString("title"),
+                    author = dr.GetString("author"),
+                    releaseDate = dr.GetDateTime("releaseDate")
+                };
+                books.Add(book);
+            }
+
+            conn.Connnection.Close();
+            return books;
+        }
+    }
+}
