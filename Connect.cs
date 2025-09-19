9600ba2e118653958018f06143edc9538f68623b
peterffydominika <peterffy.dominika@gmail.com>
2025-09-19 08:32:46 +0200

    Connection ready.

--- Connect.cs
+++ Connect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace OOP_DB
 {
     internal class Connect
     {
+        public MySqlConnection Connnection;
+        private string _host;
+        private string _database;
+        private string _user;
+        private string _password;
+        private string _connectionString;
+
+        public Connect(string database)
+        {
+            _host = "localhost";
+            _database = database;
+            _user = "root";
+            _password = "";
+
+            _connectionString = $"SERVER={_host}; DATABASE={_database}; UID={_user}; PASSWORD={_password};SslMode=None";
+
+            Connnection = new MySqlConnection(_connectionString);
+            try
+            {
+                Connnection.Open();
+                Console.WriteLine("Sikeres csatlakozás!");
+                Connnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Sikertelen csatlakozás: {ex.Message}");
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace OOP_DB
 {
@@ -11,17 +12,20 @@ namespace OOP_DB
     {
         static void Main(string[] args)
         {
-            MySqlConnection conn = new MySqlConnection("server=localhost; database=library; user=root; password=");
-            try{
-                conn.Open();
-                Console.WriteLine("Sikeres csatlakozás!");
-            }
-            catch (Exception ex){
-                Console.WriteLine($"Csatlakozás sikertelen: {ex.Message}");
-            }
-            finally{
-                conn?.Close();
-            }
+            //MySqlConnection conn = new MySqlConnection("server=localhost; database=library; user=root; password=");
+            //try{
+            //    conn.Open();
+            //    Console.WriteLine("Sikeres csatlakozás!");
+            //}
+            //catch (Exception ex){
+            //    Console.WriteLine($"Csatlakozás sikertelen: {ex.Message}");
+            //}
+            //finally{
+            //    conn?.Close();
+            //}
+            Console.WriteLine("Kérem az adatbázis nevét: ");
+            string dbName = Console.ReadLine();
+            Connect conn = new Connect(dbName);
         }
     }
 }
